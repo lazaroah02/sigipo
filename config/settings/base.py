74cd04b10a250fbf9f3af61ff6c382a3cec64c6f
TheRealVizard <vizard@divineslns.com>
2022-04-17 15:45:32 -0400

    Added postgress to github workflow

--- config/settings/base.py
+++ config/settings/base.py
@@ -78,10 +78,6 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME", None),
@@ -91,6 +87,20 @@ DATABASES = {
         "PORT": os.environ.get("DB_PORT", None),
     }
 }
+
+# Database for Github Tests
+
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "github_actions",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
