64c037fbe905c6a80736f0e542175f0f02f8dd55
TheRealVizard <vizard@divineslns.com>
2022-04-20 17:23:39 -0400

    Added test

--- apps/dashboard/test/test_views.py
+++ apps/dashboard/test/test_views.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 
 from apps.accounts.factories import UserFactory
 from apps.core.test import TestCase
+from apps.neoplasm.factories import NeoplasmFactory
+from apps.neoplasm.models import Neoplasm
 from apps.patient.factories import PatientFactory
 from apps.patient.models import Patient
 
@@ -14,11 +16,14 @@ class DashboardTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         """Common test data."""
-        Patient.objects.all().delete()
         cls.patient = PatientFactory.create(
             is_oncologic=True,
             born_municipality__province__name="Holguín",
             residence_municipality__province__name="Guantánamo",
+        )
+        cls.neoplasm = NeoplasmFactory.create(
+            patient__is_oncologic=True,
+            patient__age_at_diagnosis=19,
         )
         cls.user = UserFactory.create()
 
@@ -28,16 +33,19 @@ class DashboardTestCase(TestCase):
 
     def test_get_template(self) -> None:
         """Test that the view returns the response."""
+        Patient.objects.exclude(pk=self.patient.pk).delete()
         response = self.client.get(reverse("dashboard:dashboard"))
         self.assertIsInstance(response, TemplateResponse)
 
     def test_get_county_data(self) -> None:
         """Test that the view returns the correct data."""
+        Patient.objects.exclude(pk=self.patient.pk).delete()
         response = self.client.get(reverse("dashboard:dashboard") + "?data=county")
         self.assertIsInstance(response, JsonResponse)
 
     def test_get_born_data(self) -> None:
         """Test that the view returns the correct data."""
+        Patient.objects.exclude(pk=self.patient.pk).delete()
         response = self.client.get(reverse("dashboard:dashboard") + "?data=born")
         self.assertIsInstance(response, JsonResponse)
         data = response.json()
@@ -46,8 +54,33 @@ class DashboardTestCase(TestCase):
 
     def test_get_residence_data(self) -> None:
         """Test that the view returns the correct data."""
+        Patient.objects.exclude(pk=self.patient.pk).delete()
         response = self.client.get(reverse("dashboard:dashboard") + "?data=residence")
         self.assertIsInstance(response, JsonResponse)
         data = response.json()
         self.assertEqual(len(data), 1)
         self.assertEqual(["cu-gu", 1], data[0])
+
+    def test_get_top_ten_data(self) -> None:
+        """Test that the view returns the correct data."""
+        Neoplasm.objects.exclude(pk=self.neoplasm.pk).delete()
+        response = self.client.get(reverse("dashboard:dashboard") + "?data=top10")
+        self.assertIsInstance(response, JsonResponse)
+        data = response.json()
+        self.assertEqual(len(data), 1)
+        self.assertEqual(
+            {
+                "primary_site__code": self.neoplasm.primary_site.code,
+                "primary_site__description": self.neoplasm.primary_site.description,
+                "less_than_20": 1,
+                "num_subjects": 1,
+                "patient_in_20s": 0,
+                "patient_in_30s": 0,
+                "patient_in_40s": 0,
+                "patient_in_50s": 0,
+                "patient_in_60s": 0,
+                "patient_in_70s": 0,
+                "patient_more_than_80s": 0,
+            },
+            data[0],
+        )
--- apps/neoplasm/test/test_models.py
+++ apps/neoplasm/test/test_models.py
@@ -11,11 +11,12 @@ class NeoplasmTestCase(TestCase):
         cls.neoplasm = NeoplasmFactory.create(
             patient__first_name="Test",
             patient__last_name="Test",
+            patient__medical_record="Test",
         )
 
     def test_neoplasm_str(self):
         """Test that neoplasm str method returns the patient name."""
         self.assertEqual(
             str(self.neoplasm),
-            "Test Test",
+            "Test Test (Test)",
         )
