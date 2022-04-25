e88d1f7659cf0baf44432d238f064184f2cd7c20
TheRealVizard <vizard@divineslns.com>
2022-04-25 18:27:04 -0400

    Added migrations

--- apps/nuclear_medicine/apps.py
+++ apps/nuclear_medicine/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class NuclearMedicineConfig(AppConfig):
+    """Define the name of nuclear_medicine app."""
+
     default_auto_field = "django.db.models.BigAutoField"
-    name = "nuclear_medicine"
+    name = "apps.nuclear_medicine"
+    verbose_name = "Medicina Nuclear"
