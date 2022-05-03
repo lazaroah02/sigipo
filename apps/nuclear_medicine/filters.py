39e8364873660821fac1c0c60d7adb9134ce0a5e
TheRealVizard <vizard@divineslns.com>
2022-05-03 12:30:24 -0400

    Added filters

--- apps/nuclear_medicine/filters.py
+++ apps/nuclear_medicine/filters.py
@@ -1,7 +1,12 @@
 from django.forms import TextInput
 from django_filters import CharFilter, FilterSet
 
-from apps.nuclear_medicine.models import PatientOncologicStudy
+from apps.nuclear_medicine.models import (
+    HormonalResult,
+    OncologicResult,
+    PatientHormonalStudy,
+    PatientOncologicStudy,
+)
 
 
 class OncologicStudyFilter(FilterSet):
@@ -85,10 +90,100 @@ class HormonalStudyFilter(FilterSet):
     )
 
     class Meta:
-        model = PatientOncologicStudy
+        model = PatientHormonalStudy
         fields = [
             "patient__identity_card",
             "patient__first_name",
             "patient__last_name",
             "patient__medical_record",
+        ]
+
+
+class HormonalResultFilter(FilterSet):
+    """Filters to search for patients."""
+
+    hormonal_study__patient__identity_card = CharFilter(
+        lookup_expr="icontains",
+        widget=TextInput(
+            attrs={"class": "form-control", "placeholder": "Carnet contiene"}
+        ),
+        label="Carnet contiene",
+    )
+    hormonal_study__patient__first_name = CharFilter(
+        lookup_expr="icontains",
+        widget=TextInput(
+            attrs={"class": "form-control", "placeholder": "Nombre contiene"}
+        ),
+        label="Nombre contiene",
+    )
+    hormonal_study__patient__last_name = CharFilter(
+        lookup_expr="icontains",
+        widget=TextInput(
+            attrs={"class": "form-control", "placeholder": "Apellidos contiene"}
+        ),
+        label="Apellidos contiene",
+    )
+    hormonal_study__patient__medical_record = CharFilter(
+        lookup_expr="icontains",
+        widget=TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "No. historia clínica contiene",
+            }
+        ),
+        label="No. historia clínica contiene",
+    )
+
+    class Meta:
+        model = HormonalResult
+        fields = [
+            "hormonal_study__patient__identity_card",
+            "hormonal_study__patient__first_name",
+            "hormonal_study__patient__last_name",
+            "hormonal_study__patient__medical_record",
+        ]
+
+
+class OncologicResultFilter(FilterSet):
+    """Filters to search for patients."""
+
+    oncologic_study__patient__identity_card = CharFilter(
+        lookup_expr="icontains",
+        widget=TextInput(
+            attrs={"class": "form-control", "placeholder": "Carnet contiene"}
+        ),
+        label="Carnet contiene",
+    )
+    oncologic_study__patient__first_name = CharFilter(
+        lookup_expr="icontains",
+        widget=TextInput(
+            attrs={"class": "form-control", "placeholder": "Nombre contiene"}
+        ),
+        label="Nombre contiene",
+    )
+    oncologic_study__patient__last_name = CharFilter(
+        lookup_expr="icontains",
+        widget=TextInput(
+            attrs={"class": "form-control", "placeholder": "Apellidos contiene"}
+        ),
+        label="Apellidos contiene",
+    )
+    oncologic_study__patient__medical_record = CharFilter(
+        lookup_expr="icontains",
+        widget=TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "No. historia clínica contiene",
+            }
+        ),
+        label="No. historia clínica contiene",
+    )
+
+    class Meta:
+        model = OncologicResult
+        fields = [
+            "oncologic_study__patient__identity_card",
+            "oncologic_study__patient__first_name",
+            "oncologic_study__patient__last_name",
+            "oncologic_study__patient__medical_record",
         ]
