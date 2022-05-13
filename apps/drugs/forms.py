2bf8dac7ae87028e67205694a95095242385e7d7
TheRealVizard <vizard@divineslns.com>
2022-05-13 00:16:04 -0400

    Added form for drug

---
+++ apps/drugs/forms.py
@@ -0,0 +1,23 @@
+from django.forms import CharField, Select, TextInput
+
+from apps.core.forms import ModelForm
+from apps.drugs.models import Drug, DrugTypeChoices
+
+
+class DrugForm(ModelForm):
+    """Model to handle Drugs creation and edition."""
+
+    name = CharField(
+        widget=TextInput(attrs={"class": "form-control", "placeholder": "Nombre"}),
+        label="Nombre",
+    )
+    type = CharField(
+        widget=Select(
+            choices=DrugTypeChoices.choices,
+            attrs={"class": "form-control form-select", "placeholder": "Raza"},
+        ),
+    )
+
+    class Meta:
+        model = Drug
+        fields = "__all__"
