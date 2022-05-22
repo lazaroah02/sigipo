9da66e518376011b291caae8cdaefa8e5d2a3c64
TheRealVizard <vizard@divineslns.com>
2022-05-22 19:09:32 -0400

    Added filter

--- apps/nuclear_medicine/filters.py
+++ apps/nuclear_medicine/filters.py
@@ -2,6 +2,7 @@ from django.forms import TextInput
 from django_filters import CharFilter, FilterSet, NumberFilter
 
 from apps.nuclear_medicine.models import (
+    Gammagraphy,
     HormonalResult,
     IodineDetection,
     OncologicResult,
@@ -313,6 +314,51 @@ class IodineDetectionFilter(FilterSet):
 
     class Meta:
         model = IodineDetection
+        fields = [
+            "patient__identity_card",
+            "patient__first_name",
+            "patient__last_name",
+            "patient__medical_record",
+        ]
+
+
+class GammagraphyFilter(FilterSet):
+    """Filters to search for patients."""
+
+    patient__identity_card = CharFilter(
+        lookup_expr="icontains",
+        widget=TextInput(
+            attrs={"class": "form-control", "placeholder": "Carnet contiene"}
+        ),
+        label="Carnet contiene",
+    )
+    patient__first_name = CharFilter(
+        lookup_expr="icontains",
+        widget=TextInput(
+            attrs={"class": "form-control", "placeholder": "Nombre contiene"}
+        ),
+        label="Nombre contiene",
+    )
+    patient__last_name = CharFilter(
+        lookup_expr="icontains",
+        widget=TextInput(
+            attrs={"class": "form-control", "placeholder": "Apellidos contiene"}
+        ),
+        label="Apellidos contiene",
+    )
+    patient__medical_record = CharFilter(
+        lookup_expr="icontains",
+        widget=TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "No. historia clínica contiene",
+            }
+        ),
+        label="No. historia clínica contiene",
+    )
+
+    class Meta:
+        model = Gammagraphy
         fields = [
             "patient__identity_card",
             "patient__first_name",
