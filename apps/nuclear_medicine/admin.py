1c5f0dadb60e36d93d1f1c78ae7d08c09e75213c
TheRealVizard <vizard@divineslns.com>
2022-05-25 19:50:28 -0400

    Added admin for nuclear medicine

--- apps/nuclear_medicine/admin.py
+++ apps/nuclear_medicine/admin.py
@@ -1,7 +1,12 @@
 # Register your models here.
 from django.contrib.admin import ModelAdmin, register
 
-from apps.nuclear_medicine.models import HormonalStudy, OncologicStudy
+from apps.nuclear_medicine.models import (
+    HormonalResult,
+    HormonalStudy,
+    OncologicResult,
+    OncologicStudy,
+)
 
 
 @register(HormonalStudy)
@@ -20,3 +25,21 @@ class OncologicStudyAdmin(ModelAdmin):
     list_display = ("patient",)
     list_select_related = ("patient",)
     list_display_links = ("patient",)
+
+
+@register(HormonalResult)
+class HormonalResultAdmin(ModelAdmin):
+    """HormonalResult Django Admin view."""
+
+    list_display = ("hormonal_study",)
+    list_select_related = ("hormonal_study",)
+    list_display_links = ("hormonal_study",)
+
+
+@register(OncologicResult)
+class OncologicResultAdmin(ModelAdmin):
+    """OncologicResult Django Admin view."""
+
+    list_display = ("oncologic_study",)
+    list_select_related = ("oncologic_study",)
+    list_display_links = ("oncologic_study",)
