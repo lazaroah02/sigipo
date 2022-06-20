134541632b0539bd8d6d6a462553a0906e2a28ea
TheRealVizard <vizard@divineslns.com>
2022-06-20 13:51:41 -0400

    Added protocol model

--- apps/chemotherapy/models.py
+++ apps/chemotherapy/models.py
@@ -1,1 +1,77 @@
-# Create your models here.
+from django.db.models import (
+    CASCADE,
+    BooleanField,
+    CharField,
+    ExpressionWrapper,
+    F,
+    FloatField,
+    ForeignKey,
+    IntegerChoices,
+    IntegerField,
+    Model,
+    QuerySet,
+)
+from django.db.models.manager import Manager
+
+from apps.cancer_registry.models import NeoplasmClinicalStageChoices
+from apps.core.models import TimeStampedModel
+from apps.employee.models import Doctor
+from apps.patient.models import Patient
+
+
+class Scheme(Model):
+    name = CharField(verbose_name="Nombre", max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "Esquema"
+        verbose_name_plural = "Esquemas"
+        ordering = ["pk"]
+        default_permissions = ()
+
+
+class RoomChoices(IntegerChoices):
+    CHEMOTHERAPY = 1, "Quimioterapia"
+    ROOM_1 = 2, "Sala 1"
+
+
+class ProtocolManager(Manager):
+    def get_queryset(self) -> QuerySet:
+        return (
+            super()
+            .get_queryset()
+            .select_related("patient", "scheme", "doctor")
+            .annotate(
+                body_surface=ExpressionWrapper(
+                    round(0.007184 * (F("weight") ** 0.425) * F("height") ** 0.725, 2),
+                    output_field=FloatField(),
+                )
+            )
+        )
+
+
+class Protocol(TimeStampedModel):
+    patient = ForeignKey(Patient, verbose_name="Paciente", on_delete=CASCADE)
+    scheme = ForeignKey(Scheme, verbose_name="Esquema", on_delete=CASCADE)
+    room = IntegerField(
+        verbose_name="Lugar", null=True, blank=True, choices=RoomChoices.choices
+    )
+    height = FloatField(verbose_name="Talla")
+    weight = FloatField(verbose_name="Peso")
+    cycles = IntegerField(verbose_name="Cantidad de ciclos")
+    stage = IntegerField(
+        verbose_name="Etapa", choices=NeoplasmClinicalStageChoices.choices
+    )
+    doctor = ForeignKey(Doctor, verbose_name="Doctor", on_delete=CASCADE)
+    suspended = BooleanField()
+
+    def __str__(self):
+        return f"Protocolo de {self.patient} con esquema {self.scheme}"
+
+    class Meta:
+        verbose_name = "Protocolo"
+        verbose_name_plural = "Protocolos"
+        ordering = ["pk"]
+        default_permissions = ()
