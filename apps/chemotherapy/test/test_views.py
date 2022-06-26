6e40e2e3bb41c58f45811d59f3e2b261b716258a
TheRealVizard <vizard@divineslns.com>
2022-06-25 20:13:33 -0400

    Added test

--- apps/chemotherapy/test/test_views.py
+++ apps/chemotherapy/test/test_views.py
@@ -1,9 +1,15 @@
 from django.urls import reverse
 
 from apps.accounts.factories import UserFactory
-from apps.chemotherapy.factories import CycleFactory, CycleMedicationFactory
+from apps.chemotherapy.factories import (
+    CycleFactory,
+    CycleMedicationFactory,
+    ProtocolFactory,
+)
 from apps.chemotherapy.forms import CycleForm
+from apps.chemotherapy.models import Cycle
 from apps.core.test import TestCase
+from apps.drugs.factories import DrugFactory
 
 
 class CycleViewTestCase(TestCase):
@@ -14,13 +20,15 @@ class CycleViewTestCase(TestCase):
         """Common test data."""
         cls.cycle = CycleFactory.create()
         cls.cycle_medicine = CycleMedicationFactory.create(cycle=cls.cycle)
+        cls.protocol = ProtocolFactory.create()
+        cls.drug = DrugFactory.create()
         cls.user = UserFactory.create()
 
     def setUp(self) -> None:
         """Extra initialization."""
         self.client.force_login(self.user)
 
-    def test_cycle_create_get(self):
+    def test_cycle_get(self):
         """Test Cycle view get."""
         response = self.client.get(reverse("chemotherapy:cycle_create"))
         self.assertIsInstance(response.context["form"], CycleForm)
@@ -35,3 +43,69 @@ class CycleViewTestCase(TestCase):
         )
         self.assertIn("form", response.context)
         self.assertIn("form-check-input", response.content.decode())
+
+    def test_cycle_post(self):
+        """Test Cycle view get."""
+        cycle_count = Cycle.objects.count()
+        self.client.post(
+            reverse("chemotherapy:cycle_create"),
+            {
+                "protocol": self.protocol.pk,
+                "next_date": "2020-01-01",
+                "cyclemedication_set-0-drug": self.drug.pk,
+                "cyclemedication_set-0-dose": "1",
+                "cyclemedication_set-0-unit": "1",
+                "cyclemedication_set-TOTAL_FORMS": 1,
+                "cyclemedication_set-INITIAL_FORMS": 0,
+                "cyclemedication_set-MIN_NUM_FORMS": 0,
+                "cyclemedication_set-MAX_NUM_FORMS": 1000,
+            },
+        )
+        self.assertTrue(Cycle.objects.count() == cycle_count + 1)
+        response = self.client.post(
+            reverse("chemotherapy:cycle_create"),
+            {
+                "protocol": self.protocol.pk,
+                "next_date": "2020-01-01",
+                "cyclemedication_set-0-drug": "",
+                "cyclemedication_set-0-dose": "1",
+                "cyclemedication_set-0-unit": "1",
+                "cyclemedication_set-TOTAL_FORMS": 1,
+                "cyclemedication_set-INITIAL_FORMS": 0,
+                "cyclemedication_set-MIN_NUM_FORMS": 0,
+                "cyclemedication_set-MAX_NUM_FORMS": 1000,
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(self.cycle_medicine.drug.pk, self.drug.pk)
+        self.client.post(
+            reverse("chemotherapy:cycle_update", args=(self.cycle.pk,)),
+            {
+                "protocol": self.cycle.protocol.pk,
+                "next_date": "2020-01-01",
+                "cyclemedication_set-0-id": self.cycle_medicine.pk,
+                "cyclemedication_set-0-drug": self.drug.pk,
+                "cyclemedication_set-0-cycle": self.cycle.pk,
+                "cyclemedication_set-0-dose": "1",
+                "cyclemedication_set-0-unit": "1",
+                "cyclemedication_set-TOTAL_FORMS": 2,
+                "cyclemedication_set-INITIAL_FORMS": 1,
+                "cyclemedication_set-MIN_NUM_FORMS": 0,
+                "cyclemedication_set-MAX_NUM_FORMS": 1000,
+            },
+        )
+        self.cycle_medicine.refresh_from_db()
+        self.assertEqual(self.cycle_medicine.drug.pk, self.drug.pk)
+        self.client.post(
+            reverse("chemotherapy:cycle_update", args=(self.cycle.pk,)),
+            {
+                "protocol": self.cycle.protocol.pk,
+                "next_date": "2020-01-01",
+                "cyclemedication_set-0-id": self.cycle_medicine.pk,
+                "cyclemedication_set-0-drug": self.drug.pk,
+                "cyclemedication_set-0-cycle": self.cycle.pk,
+                "cyclemedication_set-0-dose": "1",
+                "cyclemedication_set-MAX_NUM_FORMS": 1000,
+            },
+        )
+        self.assertEqual(response.status_code, 200)
