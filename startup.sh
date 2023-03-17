341c34698acbead34d76519e80e15f359ca1f47e
TheRealVizard <vizard@divineslns.com>
2023-03-17 19:51:57 -0400

    staticfiles test2

--- startup.sh
+++ startup.sh
@@ -1,5 +1,3 @@
-python manage.py collectstatic --no-input --clear
-python manage.py migrate
 gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
     '-' --error-logfile '-' --bind=0.0.0.0:8000 \
      --chdir=/home/site/wwwroot config.wsgi
