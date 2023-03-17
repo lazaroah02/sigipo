debbeddc8d24c833add508053c25363fbf3c0657
TheRealVizard <vizard@divineslns.com>
2023-03-17 19:43:45 -0400

    staticfiles test1

--- config/settings/base.py
+++ config/settings/base.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).parent.parent.parent
 
 load_dotenv(dotenv_path=BASE_DIR / ".env")
 
--- config/settings/production.py
+++ config/settings/production.py
@@ -2,13 +2,15 @@
 from config.settings.base import *  # unimport:skip
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://sigipo.azurewebsites.net"]
+
 DEBUG = False
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "sigipo", "static"),)
-STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_URL = "/static/"
-CSRF_TRUSTED_ORIGINS = ["https://sigipo.azurewebsites.net"]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATICFILES_DIRS = [str(BASE_DIR / "sigipo" / "static" )]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
