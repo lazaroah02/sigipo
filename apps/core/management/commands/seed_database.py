cd59d24b11676f3fc363f126e8a4a1d5772cff2c
Rodolfo Becerra <44782644+rodolvbg@users.noreply.github.com>
2023-04-17 17:06:07 +0200

    Seed database changes (#105)
    * remove redundant subfactory variables
    
    * check instances count dynamically
    
    * notify to the user that the seed finished

--- apps/core/management/commands/seed_database.py
+++ apps/core/management/commands/seed_database.py
@@ -4,10 +4,8 @@ from apps.accounts.factories import UserFactory
 from apps.cancer_registry.factories import (
     DoctorFactory,
     GroupFactory,
-    MorphologyFactory,
     NeoplasmFactory,
     PatientFactory,
-    TopographyFactory,
 )
 from apps.chemotherapy.factories import (
     CycleFactory,
@@ -15,21 +13,20 @@ from apps.chemotherapy.factories import (
     DrugFactory,
     MedicationFactory,
     ProtocolFactory,
-    SchemeFactory,
 )
-from apps.geographic_location.factories import MunicipalityFactory, ProvinceFactory
+from apps.geographic_location.factories import MunicipalityFactory
 from apps.nuclear_medicine.factories import (
     GammagraphyFactory,
     HormonalResultFactory,
     HormonalStudyFactory,
     IodineDetectionFactory,
-    NuclearMedicineDrugFactory,
     OncologicResultFactory,
     OncologicStudyFactory,
-    RadioIsotopeFactory,
     SerialIodineDetectionFactory,
     StudyFactory,
 )
+
+NUMBER_OF_INSTANCES_TO_CREATE = 5
 
 
 class Command(BaseCommand):
@@ -45,31 +42,21 @@ class Command(BaseCommand):
         default handle method to execute the management command functionality.
         """
         UserFactory.create(username="admin", password="123")
-        for _ in range(5):
-            province = ProvinceFactory.create()
-            municipality = MunicipalityFactory.create(province=province)
+        for _ in range(NUMBER_OF_INSTANCES_TO_CREATE):
+            municipality = MunicipalityFactory.create()
             patient = PatientFactory.create(
                 residence_municipality=municipality, born_municipality=municipality
             )
             group = GroupFactory.create()
             doctor = DoctorFactory.create(group=group)
-            topography = TopographyFactory.create()
-            morphology = MorphologyFactory.create()
             study = StudyFactory.create()
-            radioisotope = RadioIsotopeFactory.create()
             NeoplasmFactory.create(
                 patient=patient,
-                primary_site=topography,
-                histologic_type=morphology,
                 group=group,
                 medic_that_report=doctor,
             )
-            scheme = SchemeFactory.create()
-            protocol = ProtocolFactory.create(
-                patient=patient, scheme=scheme, doctor=doctor
-            )
+            protocol = ProtocolFactory.create(patient=patient, doctor=doctor)
             drug = DrugFactory.create()
-            nuclearmedicinedrug = NuclearMedicineDrugFactory.create()
             MedicationFactory.create(drug=drug, protocol=protocol)
             cycle = CycleFactory.create(protocol=protocol)
             CycleMedicationFactory.create(cycle=cycle, drug=drug)
@@ -81,7 +68,6 @@ class Command(BaseCommand):
             SerialIodineDetectionFactory.create(patient=patient)
             GammagraphyFactory.create(
                 patient=patient,
-                drug=nuclearmedicinedrug,
-                radio_isotope=radioisotope,
                 requested_study=[study],
             )
+        self.stdout.write("Created a samples of data.")
--- apps/core/tests/test_seed_database.py
+++ apps/core/tests/test_seed_database.py
@@ -1,31 +1,11 @@
 from io import StringIO
 
+from django.apps import apps
 from django.core.management import call_command
 
 from apps.accounts.models import User
-from apps.cancer_registry.models import Neoplasm
-from apps.chemotherapy.models import (
-    Cycle,
-    CycleMedication,
-    Medication,
-    Protocol,
-    Scheme,
-)
-from apps.classifiers.models import Morphology, RadioIsotope, Study, Topography
+from apps.core.management.commands.seed_database import NUMBER_OF_INSTANCES_TO_CREATE
 from apps.core.test import TestCase
-from apps.drugs.models import Drug, NuclearMedicineDrug
-from apps.employee.models import Doctor, Group
-from apps.geographic_location.models import Province
-from apps.nuclear_medicine.models import (
-    Gammagraphy,
-    HormonalResult,
-    HormonalStudy,
-    IodineDetection,
-    OncologicResult,
-    OncologicStudy,
-    SerialIodineDetection,
-)
-from apps.patient.models import Municipality, Patient
 
 
 class SeedDatabaseTest(TestCase):
@@ -42,54 +22,14 @@ class SeedDatabaseTest(TestCase):
 
     def test_province_instances_number(self):
         self.call_command()
-        user = User.objects.all().count()
-        provinces = Province.objects.all().count()
-        municipality = Municipality.objects.all().count()
-        patient = Patient.objects.all().count()
-        group = Group.objects.all().count()
-        doctor = Doctor.objects.all().count()
-        topography = Topography.objects.all().count()
-        morphology = Morphology.objects.all().count()
-        study = Study.objects.all().count()
-        radioisotope = RadioIsotope.objects.all().count()
-        neoplasm = Neoplasm.objects.all().count()
-        scheme = Scheme.objects.all().count()
-        protocol = Protocol.objects.all().count()
-        drug = Drug.objects.all().count()
-        nuclearmedicinedrug = NuclearMedicineDrug.objects.all().count()
-        medication = Medication.objects.all().count()
-        cycle = Cycle.objects.all().count()
-        cyclemedication = CycleMedication.objects.all().count()
-        oncologicstudy = OncologicStudy.objects.all().count()
-        hormonalstudy = HormonalStudy.objects.all().count()
-        oncologicresult = OncologicResult.objects.all().count()
-        hormonalresult = HormonalResult.objects.all().count()
-        iodinedetection = IodineDetection.objects.all().count()
-        serialiodinedetection = SerialIodineDetection.objects.all().count()
-        gammagraphy = Gammagraphy.objects.all().count()
-
-        self.assertEqual(provinces, 5, "Provincias")
-        self.assertEqual(municipality, 5, "Municipios")
-        self.assertEqual(patient, 5, "Pacientes")
-        self.assertEqual(group, 5, "Grupos")
-        self.assertEqual(doctor, 5, "Doctor")
-        self.assertEqual(topography, 5, "Topografía")
-        self.assertEqual(morphology, 5, "Morfología")
-        self.assertEqual(study, 5, "Estudios")
-        self.assertEqual(radioisotope, 5, "Radioisótopos")
-        self.assertEqual(neoplasm, 5, "Neoplastias")
-        self.assertEqual(scheme, 5, "Esquemas")
-        self.assertEqual(protocol, 5, "Protocolos")
-        self.assertEqual(drug, 5, "Fármacos")
-        self.assertEqual(nuclearmedicinedrug, 5, "Fármacos-MN")
-        self.assertEqual(medication, 5, "Medicación")
-        self.assertEqual(cycle, 5, "Ciclo")
-        self.assertEqual(cyclemedication, 5, "Cyclos de Medicación")
-        self.assertEqual(oncologicstudy, 5, "Estudio Oncológico")
-        self.assertEqual(hormonalstudy, 5, "Estudio Hormonal")
-        self.assertEqual(oncologicresult, 5, "Resultados Oncológicos")
-        self.assertEqual(hormonalresult, 5, "Resultados Hormonales")
-        self.assertEqual(user, 1, "Usuario")
-        self.assertEqual(iodinedetection, 5, "Detección de Yodo")
-        self.assertEqual(serialiodinedetection, 5, "Detección de Yodo Seriada")
-        self.assertEqual(gammagraphy, 5, "Gammagrafías")
+        exempt_apps = ("accounts", "dashboard")
+        for app_config in apps.get_app_configs():
+            if (
+                app_config.name.startswith("apps.")
+                and app_config.label not in exempt_apps
+            ):
+                for model_name, model in app_config.models.items():
+                    self.assertEqual(
+                        model.objects.count(), NUMBER_OF_INSTANCES_TO_CREATE, model_name
+                    )
+        self.assertEqual(User.objects.count(), 1, "user")
