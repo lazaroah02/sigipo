aa91630b150309a35942667369df7d4e1a3ad3e7
Arlan <arlan010105@gmail.com>
2023-11-05 23:24:16 -0500

    Creating TAC Request model prototype

--- apps/radiotherapy/admin.py
+++ apps/radiotherapy/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
+
+from apps.radiotherapy.models import (
+    Physicist,
+    TACRequest,
+)
 
 # Register your models here.
+
+@register(Physicist)
+class PhysicistAdmin(ModelAdmin):
+    """
+    Physicist Django Admin view.
+    """
+
+    pass
+
+@register(TACRequest)
+class TACRequestAdmin(ModelAdmin):
+    """
+    TACRequest Django Admin view.
+    """
+
+    pass
